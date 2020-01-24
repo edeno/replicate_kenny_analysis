29ac8e3125729a5680aa0cc28bff81d8d5e37a58
Eric Denovellis <edeno@bu.edu>
2020-01-24 14:38:21 -0800

    Add code for loading data and parameters.

---
+++ src/load_data.py
@@ -0,0 +1,150 @@
+from logging import getLogger
+
+import numpy as np
+import pandas as pd
+from loren_frank_data_processing import (get_all_multiunit_indicators,
+                                         get_all_spike_indicators,
+                                         get_interpolated_position_dataframe,
+                                         get_LFPs, get_trial_time,
+                                         make_neuron_dataframe,
+                                         make_tetrode_dataframe)
+from ripple_detection import (Kay_ripple_detector, filter_ripple_band,
+                              get_multiunit_population_firing_rate)
+
+from .parameters import _MARKS, ANIMALS, BRAIN_AREAS, SAMPLING_FREQUENCY
+
+logger = getLogger(__name__)
+
+
+def get_labels(times, time):
+    ripple_labels = pd.DataFrame(np.zeros_like(time, dtype=np.int), index=time,
+                                 columns=['replay_number'])
+    for replay_number, start_time, end_time in times.itertuples():
+        ripple_labels.loc[start_time:end_time] = replay_number
+
+    return ripple_labels
+
+
+def get_ripple_times(epoch_key, sampling_frequency=1500,
+                     brain_areas=BRAIN_AREAS):
+    position_info = (
+        get_interpolated_position_dataframe(epoch_key, ANIMALS)
+        .dropna(subset=['linear_distance', 'linear_speed']))
+    speed = position_info['speed']
+    time = position_info.index
+    tetrode_info = make_tetrode_dataframe(ANIMALS).xs(
+        epoch_key, drop_level=False)
+    if ~np.all(np.isnan(tetrode_info.validripple.astype(float))):
+        tetrode_keys = tetrode_info.loc[
+            (tetrode_info.validripple == 1)].index
+    else:
+        is_brain_areas = (
+            tetrode_info.area.astype(str).str.upper().isin(brain_areas))
+        tetrode_keys = tetrode_info.loc[is_brain_areas].index
+
+    lfps = get_LFPs(tetrode_keys, ANIMALS).reindex(time)
+    return Kay_ripple_detector(
+        time, lfps.values, speed.values, sampling_frequency,
+        zscore_threshold=2.0, close_ripple_threshold=np.timedelta64(0, 'ms'),
+        minimum_duration=np.timedelta64(15, 'ms'))
+
+
+def get_adhoc_ripple(time, speed, tetrode_info):
+    is_brain_areas = (
+        tetrode_info.area.astype(str).str.upper().isin(BRAIN_AREAS))
+    tetrode_keys = tetrode_info.loc[
+        is_brain_areas & (tetrode_info.validripple == 1)].index
+
+    ripple_lfps = get_LFPs(tetrode_keys, ANIMALS).reindex(time)
+    ripple_lfps = (
+        ripple_lfps.resample('1ms').mean().fillna(method='pad').reindex(time))
+    ripple_times = Kay_ripple_detector(
+        time, ripple_lfps.values, speed.values, SAMPLING_FREQUENCY,
+        zscore_threshold=2.0, close_ripple_threshold=np.timedelta64(0, 'ms'),
+        minimum_duration=np.timedelta64(15, 'ms'))
+
+    ripple_times.index = ripple_times.index.rename('replay_number')
+    ripple_labels = get_labels(ripple_times, time)
+    is_ripple = ripple_labels > 0
+
+    return dict(ripple_times=ripple_times,
+                is_ripple=is_ripple)
+
+
+def load_data(epoch_key, brain_areas=None):
+
+    if brain_areas is None:
+        brain_areas = BRAIN_AREAS
+
+    time = get_trial_time(epoch_key, ANIMALS)
+    time = (pd.Series(np.ones_like(time, dtype=np.float), index=time)
+            .resample('2ms').mean()
+            .index)
+
+    def _time_function(*args, **kwargs):
+        return time
+
+    position_info = (
+        get_interpolated_position_dataframe(
+            epoch_key, ANIMALS, _time_function)
+        .dropna(subset=['linear_distance', 'linear_speed']))
+
+    time = position_info.index
+
+    tetrode_info = make_tetrode_dataframe(ANIMALS).xs(
+        epoch_key, drop_level=False)
+    is_brain_areas = (
+        tetrode_info.area.astype(str).str.upper().isin(brain_areas))
+    tetrode_keys = tetrode_info.loc[is_brain_areas].index
+    lfps = get_LFPs(tetrode_keys, ANIMALS)
+    lfps = lfps.resample('2ms').mean().fillna(method='pad').reindex(time)
+
+    try:
+        neuron_info = make_neuron_dataframe(ANIMALS).xs(
+            epoch_key, drop_level=False)
+        neuron_info = neuron_info.loc[
+            (neuron_info.numspikes > 100) &
+            neuron_info.area.isin(brain_areas) &
+            (neuron_info.type == 'principal')]
+        spikes = get_all_spike_indicators(
+            neuron_info.index, ANIMALS, _time_function).reindex(time)
+    except KeyError:
+        spikes = None
+
+    tetrode_info = tetrode_info.loc[is_brain_areas]
+    multiunit = (get_all_multiunit_indicators(
+        tetrode_info.index, ANIMALS, _time_function)
+        .sel(features=_MARKS)
+        .reindex({'time': time}))
+    multiunit_spikes = (np.any(~np.isnan(multiunit.values), axis=1)
+                        ).astype(np.float)
+    multiunit_firing_rate = pd.DataFrame(
+        get_multiunit_population_firing_rate(
+            multiunit_spikes, SAMPLING_FREQUENCY), index=time,
+        columns=['firing_rate'])
+
+    logger.info('Finding ripple times...')
+    ripple_times = get_ripple_times(epoch_key)
+
+    ripple_band_lfps = pd.DataFrame(
+        np.stack([filter_ripple_band(lfps.values[:, ind])
+                  for ind in np.arange(lfps.shape[1])], axis=1),
+        index=lfps.index)
+
+    is_ripple = pd.DataFrame(np.zeros_like(time, dtype=np.int), index=time,
+                             columns=['ripple_number'])
+    for replay_number, start_time, end_time in ripple_times.itertuples():
+        is_ripple.loc[start_time:end_time] = replay_number
+
+    return {
+        'position_info': position_info,
+        'ripple_times': ripple_times,
+        'spikes': spikes,
+        'multiunit': multiunit,
+        'lfps': lfps,
+        'tetrode_info': tetrode_info,
+        'ripple_band_lfps': ripple_band_lfps,
+        'multiunit_firing_rate': multiunit_firing_rate,
+        'sampling_frequency': SAMPLING_FREQUENCY,
+        'is_ripple': is_ripple,
+    }
---
+++ src/parameters.py
@@ -0,0 +1,34 @@
+from os.path import abspath, dirname, join, pardir
+
+import numpy as np
+from loren_frank_data_processing import Animal
+
+# LFP sampling frequency
+SAMPLING_FREQUENCY = 500
+
+# Data directories and definitions
+ROOT_DIR = join(abspath(dirname(__file__)), pardir)
+RAW_DATA_DIR = join(ROOT_DIR, 'Raw-Data')
+PROCESSED_DATA_DIR = join(ROOT_DIR, 'Processed-Data')
+FIGURE_DIR = join(ROOT_DIR, 'figures')
+
+ANIMALS = {
+    'bon': Animal(directory=join(RAW_DATA_DIR, 'Bond'), short_name='bon'),
+    'fra': Animal(directory=join(RAW_DATA_DIR, 'Frank'), short_name='fra'),
+}
+
+_MARKS = ['channel_1_max', 'channel_2_max', 'channel_3_max', 'channel_4_max']
+BRAIN_AREAS = ['CA1', 'CA2', 'CA3']
+
+detector_parameters = {
+    'movement_var': 25.0,
+    'replay_speed': 1,
+    'place_bin_size': 1.0,
+    'spike_model_knot_spacing': 8.0,
+    'spike_model_penalty': 0.5,
+    'movement_state_transition_type': 'w_track_1D_random_walk',
+    'multiunit_model_kwargs': {
+        'bandwidth': np.array([20.0, 20.0, 20.0, 20.0, 8.0])},
+    'multiunit_occupancy_kwargs': {'bandwidth': np.array([8.0])},
+    'discrete_state_transition_type': 'constant',
+}
